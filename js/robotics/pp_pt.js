f53c3ffcf2fe2767c998743f87401ce283c7e025
linzhibo <zlin@observerai.com>
2022-05-09 15:25:39 +0800

    Add path planning, path tracking demo

--- js/robotics/pp_pt.js
+++ js/robotics/pp_pt.js
@@ -1,6 +1,6 @@
 
+// GLOBAL variables 
 var canvas = document.getElementById('canvas');
-// document.body.appendChild(canvas);
 
 // some hotfixes... ( ≖_≖)
 document.body.style.margin = 0;
@@ -10,15 +10,15 @@ canvas.style.position = 'fixed';
 var ctx = canvas.getContext('2d');
 resize();
 
-var car = new Car(50, 50, "gray");
-var arrow_head_pose = new Pose2D(0,0,0);
+var car = new Car(ctx, 50, 50, "gray");
+var arrow_head_pose = new Pose2D(0, 0, 0);
+var animation_id = -1;
 var goal_pose = new Pose2D(0,0,0);
 var start_pose = new Pose2D(0,0,0);
 var path = new Path(start_pose, goal_pose, arrow_head_pose);
+var path_tracker = new PurePursuit();
 
-window.addEventListener('resize', resize);
 document.addEventListener('mousedown', setGoalPosition);
-document.addEventListener('mouseenter', setPosition);
 document.addEventListener('mouseup', path_plan);
 document.addEventListener('mousemove', draw);
 
@@ -45,28 +45,38 @@ function setPosition(e) {
 }
 
 function path_plan(e) {
+  path_tracker.reset();
   setStartPosition(car.x, car.y, car.yaw);
   setGoalYaw(goal_pose, arrow_head_pose);
   let car_curvature = Math.tan(car.max_steer) / car.wheel_base
-  path = reeds_shepp_path_planning( start_pose, goal_pose, car_curvature, 1)
+  path = reeds_shepp_path_planning(start_pose, goal_pose, car_curvature, 1);
+  if (path == undefined) {
+    console.log("failed to plan" );
+    return;
+  }
+  if (animation_id != -1) {
+    window.cancelAnimationFrame(animation_id);
+  }
+  
   track();
 }
 
-function track(){
+function track() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // car.update(0.5, 0.1, 0.1);
+  let arrived = false;
+  [pedal, steer, arrived] = path_tracker.compute_cmd(car, path);
+  car.update(pedal, steer, 0.1, arrived);
   car.render();
-  canvas_arrow(ctx, goal_pose.x, goal_pose.y, arrow_head_pose.x, arrow_head_pose.y);
   path.render();
-  raf = window.requestAnimationFrame(track);
+  canvas_arrow(ctx, goal_pose.x, goal_pose.y, arrow_head_pose.x, arrow_head_pose.y);
+  canvas_cross(ctx, path.x[path_tracker.current_idx], path.y[path_tracker.current_idx]);
+  animation_id = window.requestAnimationFrame(track);
 }
 
-
-//#############################################################
 // resize canvas
 function resize() {
   ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+  // ctx.canvas.height = window.innerHeight;
 }
 
 function draw(e) {
@@ -74,6 +84,7 @@ function draw(e) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   car.render();
   car.reset_speed();
+  car.reset_history_points();
   canvas_arrow(ctx, goal_pose.x, goal_pose.y, arrow_head_pose.x, arrow_head_pose.y);
   setPosition(e);
 }
